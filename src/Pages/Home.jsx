46f31e522e3a3e8e3ea0ada056fcc4eadb4f3618
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-06-10 17:15:07 -0500

    Feat: Home page fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,12 +11,12 @@ export const Home = () => {
           <img src="https://www.dhla.org/wp-content/uploads/2019/08/LOGO-uvonhumboldt.png" alt="Logo CUE" />
         </div>
         <div className="text-center">
-          <p className="text-cue-white font-bold text-[50px]">Programa de Administración de Empresas Dual</p>
+          <p className="text-cue-white font-bold text-[50px]">Nombre del Juego - Programa de Administración de Empresas Dual</p>
         </div>
       </div>
-      <Button className="text-cue-white text-[30px]" color="green">
-        <NavLink to='/product-design'>Iniciar Juego</NavLink>
-      </Button>
+      <NavLink to='/product-design'>
+        <Button className="text-cue-white text-[30px]" color="green">Iniciar Juego</Button>
+      </NavLink>
     </div>
   )
 }
