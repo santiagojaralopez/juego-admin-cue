bb7e156ac64b5edda1a5aa224737511539ef21bd
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-06-10 22:30:03 -0500

    Feat: changed budget bg-color

--- src/Components/Budget.jsx
+++ src/Components/Budget.jsx
@@ -1,13 +1,13 @@
 import { useContext } from "react"
 
-import { BudgetContext } from "../Context/BudgetContext"
+import { Context } from "../Context/Context"
 
 export const Budget = () => {
-  const context = useContext(BudgetContext);
+  const context = useContext(Context);
 
   return (
     <nav className="flex justify-end">
-      <div className="p-5 mr-10 flex items-center justify-center bg-cue-red rounded-b-3xl">
+      <div className="p-5 mr-10 flex items-center justify-center bg-cue-blue rounded-b-3xl">
         <p className="font-cocogoose text-cue-white text-[20px]">
           Presupuesto disponible: ${context.budget}
         </p>
