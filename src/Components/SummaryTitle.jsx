2d2bdf098a347659b7ca2bb4f267bfa08fbc09f4
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-06-11 21:00:41 -0500

    Feat: added summaryTitle

---
+++ src/Components/SummaryTitle.jsx
@@ -0,0 +1,11 @@
+export const SummaryTitle = () => {
+  return (
+    <nav className="flex justify-center">
+      <div className="px-[150px] py-5 mr-10 flex items-center justify-center bg-cue-blue rounded-b-3xl">
+        <p className="font-cocogoose text-cue-white text-[20px]">
+          Resumen de Costos
+        </p>
+      </div>
+    </nav>
+  )
+}
